b60435a8a833b8e56c2f2a6ea3f79e18a0302137
rki <rki@sbs.co.at>
2025-03-14 19:00:06 +0100

    implement function fibonacci

--- test.py
+++ test.py
@@ -4,3 +4,15 @@ def sum_by_raphael(a, b):
 
 # Egon hat es vielleicht doch geschafft..
 c = input(int("Geben Sie einen Wert für C ein:"))
+
+## lets implement something stupid
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+print(fibonacci(c))
