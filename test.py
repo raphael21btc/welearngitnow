4795f0bd345909412bb9847ecd537157f6947388
rki <rki@sbs.co.at>
2025-03-14 19:03:42 +0100

    implement quick sort

--- test.py
+++ test.py
@@ -16,3 +16,13 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
     
 print(fibonacci(c))
+
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
