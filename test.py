a683efc53bb877c6499884b240a60c13d89764de
EgonV80 <egon@vizauer.at>
2025-03-15 08:20:43 +0100

    Guten Morgen

--- test.py
+++ test.py
@@ -16,18 +16,5 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
     
-print(fibonacci(c))
 
-
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
-
-
-def by_michael(x):
-    pass
+print ("Guten Morgen!")
